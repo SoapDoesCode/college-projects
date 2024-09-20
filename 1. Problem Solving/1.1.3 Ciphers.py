0b2d4056a466c71d385b5d4df74bf62e3746092e
SoapDoesCode <86624968+SoapDoesCode@users.noreply.github.com>
2024-09-20 10:44:27 +0100

    fixed Caesar Cipher code

--- 1. Problem Solving/1.1.3 Ciphers.py
+++ 1. Problem Solving/1.1.3 Ciphers.py
@@ -8,14 +8,16 @@ def atbash_cipher(text):
     result = ''.join(atbash_dict.get(char, char) for char in text.upper())
     return result
 
-# DOES NOT FUCKING WORK
 def caesar_cipher(text, shift):
-    alphabet = string.ascii_lowercase
+    alphabet = string.ascii_letters
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     print(shifted_alphabet)
     caesar_dict = dict(zip(alphabet, shifted_alphabet))
-    result = ''.join(caesar_cipher.get(char, char) for char in text.upper())
+    result = ''.join(caesar_dict.get(char, char) for char in text.upper())
+    return result
 
 while True:
     text = input("Text to encode/decode: ")
-    print(f"Output: {atbash_cipher(text)}")
+    print(f"Atbash: {atbash_cipher(text)}")
+
+    print(f"Caesar: {caesar_cipher(text, 13)}")
