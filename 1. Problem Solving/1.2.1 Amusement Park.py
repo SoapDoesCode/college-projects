09dee4f27253192bce946a07992a9f3c931e35ed
SoapDoesCode <86624968+SoapDoesCode@users.noreply.github.com>
2024-09-27 08:19:06 +0100

    Update 1.2.1 Amusement Park.py to add error handling

--- 1. Problem Solving/1.2.1 Amusement Park.py
+++ 1. Problem Solving/1.2.1 Amusement Park.py
@@ -18,12 +18,15 @@ while True:
     print(f"Current balance: £{card_balance}")
     card_balance += float(input("How much do you want to put on your card?\nAmount to add: £")) # asks the user for their balance
     print(f"Your new balance: £{card_balance}")
-    ride_choice = input("Please choose a ride\nRide: ").lower()
+    ride_choice = input("Please choose a ride\nRide name: ").lower()
     
-    # compares the card balance to the ride fee
-    if card_balance >= rides[ride_choice]: # if they have enough:
-        card_balance -= rides[ride_choice] # subtract ride fee from card
-        print(f"Your new balance: £{card_balance}\nOpening barrier...") # output their new balance and open barrier
-    else:
-        # if they do not have enough, ask them to top up and then their balance will be output at the loop start
-        print("You do not have high enough balance on your card, please add more")
+    try:
+        # compares the card balance to the ride fee
+        if card_balance >= rides[ride_choice]: # if they have enough:
+            card_balance -= rides[ride_choice] # subtract ride fee from card
+            print(f"Your new balance: £{card_balance}\nOpening barrier to {ride_choice}...") # output their new balance and open barrier
+        else:
+            # if they do not have enough, ask them to top up and then their balance will be output at the loop start
+            print("You do not have high enough balance on your card, please add more")
+    except:
+        print(f"Invalid ride choice: {ride_choice}")
